3b37adde920d250a758601d0d2992065ee95ddfd
VHall1 <victorhallpsn@hotmail.com>
2024-04-19 23:29:56 +0100

    stuff

---
+++ app/routes/todos/route.tsx
@@ -0,0 +1,91 @@
+import { ActionFunctionArgs, json } from "@remix-run/node";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import { Fragment } from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group";
+import { prisma } from "~/services/database.server";
+import { TodoItem } from "./todo-item";
+
+export default function Todos() {
+  const { todos, itemsLeft } = useLoaderData<typeof loader>();
+  const createFetcher = useFetcher();
+
+  return (
+    <div className="container max-w-screen-2xl">
+      <h1 className="text-center text-3xl font-semibold tracking-tight">
+        Todos
+      </h1>
+
+      <Card className="md:max-w-xl mx-auto mt-6">
+        <CardHeader>
+          <createFetcher.Form method="post">
+            <input type="hidden" name="intent" value="create" />
+            <Input placeholder="What needs to be done?" name="content" />
+          </createFetcher.Form>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-2">
+            {todos.map((todo) => (
+              <Fragment key={`todo-${todo.id}`}>
+                <TodoItem todo={todo} />
+              </Fragment>
+            ))}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="flex flex-1 items-center justify-between">
+            <span>{itemsLeft} items left!</span>
+            <ToggleGroup type="single">
+              <ToggleGroupItem value="all">All</ToggleGroupItem>
+              <ToggleGroupItem value="active">Active</ToggleGroupItem>
+              <ToggleGroupItem value="completed">Completed</ToggleGroupItem>
+            </ToggleGroup>
+            <Button variant="ghost">Clear completed</Button>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export const loader = async () => {
+  const todos = await prisma.todo.findMany();
+  const itemsLeft = todos.filter((todo) => !todo.completed).length;
+  return json({ todos, itemsLeft });
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  // TODO: validate input
+  const formData = await request.formData();
+
+  const intent = formData.get("intent");
+  switch (intent) {
+    case "toggle": {
+      const id = formData.get("id")?.toString();
+      await prisma.todo.update({
+        where: { id },
+        data: { completed: formData.get("next") === "true" },
+      });
+      return json({ ok: true });
+    }
+    case "create": {
+      const content = formData.get("content")?.toString() || "";
+      const todo = await prisma.todo.create({ data: { content } });
+      return json({ todo }, 201);
+    }
+    case "delete": {
+      const id = formData.get("id")?.toString();
+      await prisma.todo.delete({ where: { id } });
+      return new Response(undefined, { status: 204 });
+    }
+  }
+
+  return null;
+};
---
+++ app/routes/todos/todo-item.tsx
@@ -0,0 +1,55 @@
+import { Todo } from "@prisma/client";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { SerializeFrom } from "@remix-run/node";
+import { useFetcher } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+import { Checkbox } from "~/components/ui/checkbox";
+import { cn } from "~/utils/cn";
+
+export function TodoItem({ todo }: { todo: SerializeFrom<Todo> }) {
+  const toggleFetcher = useFetcher();
+  const deleteFetcher = useFetcher();
+
+  const toggleFormId = `toggle-form-${todo.id}`;
+  const deleteFormId = `delete-form-${todo.id}`;
+
+  return (
+    <>
+      <toggleFetcher.Form id={toggleFormId} method="post" />
+      <deleteFetcher.Form id={deleteFormId} method="post" />
+
+      <input type="hidden" name="intent" value="toggle" form={toggleFormId} />
+      <input type="hidden" name="id" value={todo.id} form={toggleFormId} />
+      <input
+        type="hidden"
+        name="next"
+        value={(!todo.completed).toString()}
+        form={toggleFormId}
+      />
+
+      <div className="flex items-center">
+        <label
+          htmlFor={`task-${todo.id}`}
+          className={cn("flex items-center flex-1", {
+            "line-through": todo.completed,
+          })}
+        >
+          <Checkbox
+            id={`task-${todo.id}`}
+            defaultChecked={todo.completed}
+            className="mr-2"
+            type="submit"
+            form={toggleFormId}
+          />
+          {todo.content}
+        </label>
+
+        <input type="hidden" name="intent" value="delete" form={deleteFormId} />
+        <input type="hidden" name="id" value={todo.id} form={deleteFormId} />
+        <Button size="icon" variant="ghost" form={deleteFormId}>
+          <TrashIcon className="h-5 w-5" />
+        </Button>
+      </div>
+    </>
+  );
+}
