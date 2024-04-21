c204fd80932e50b560658b98590c62cd2fce2f1a
VHall1 <victorhallpsn@hotmail.com>
2024-04-21 21:37:05 +0100

    supress hidration warnings on body

--- app/root.tsx
+++ app/root.tsx
@@ -8,32 +8,29 @@ import {
   useLoaderData,
 } from "@remix-run/react";
 import { ReactNode } from "react";
-import { createHead } from "remix-island";
 import { getUser } from "./services/session.server";
 import "./tailwind.css";
-
-export const Head = createHead(() => (
-  <>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <Meta />
-    <Links />
-  </>
-));
 
 export function Layout({ children }: { children: ReactNode }) {
   const { sha } = useLoaderData<typeof loader>();
 
   return (
-    <>
-      <Head />
-      <div id="git-revision" className="hidden">
-        {sha}
-      </div>
-      <div className="h-screen">{children}</div>
-      <ScrollRestoration />
-      <Scripts />
-    </>
+    <html lang="en" className="dark">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-screen" suppressHydrationWarning>
+        <div id="git-revision" className="hidden">
+          {sha}
+        </div>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
   );
 }
 
