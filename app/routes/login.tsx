2bb8c9b2f7a490dbf0435ff31f5eeb331acd17b7
VHall1 <victorhallpsn@hotmail.com>
2024-04-28 00:42:38 +0100

    remove onBlur validate

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -24,7 +24,6 @@ export default function Login() {
     onValidate({ formData }) {
       return parseWithZod(formData, { schema });
     },
-    shouldValidate: "onBlur",
   });
 
   return (
--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -25,7 +25,6 @@ export default function Signup() {
     onValidate({ formData }) {
       return parseWithZod(formData, { schema });
     },
-    shouldValidate: "onBlur",
   });
 
   return (
